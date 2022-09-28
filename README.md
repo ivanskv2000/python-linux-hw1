c486f78ae7318aa306eadbcc1d089593a2272847
Ivan Skvortsov <iaskvortsov@avito.ru>
2022-09-28 13:01:05 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# python-linux-hw1
+# Python and Linux &mdash; HW-1
+
+## Description
+This script parses a website up to a specified depth or number of pages.
+
+By default, parser.py creates an url.txt file with page ids and their urls
+and a data folder with html contents of pages (<page id>.html). If -t option is enabled, tree.txt file is also created,
+which contains a tree representation of the website.
+
+## Usage
+### Step 1. Set Up the Working Environment
+To isolate project execution, first create and activate a python virtual environment in the project directory:
+``` bash
+cd path_to_project/
+python -m venv ./venv
+source venv/bin/activate 
+```
+
+This enables you to install all required packages from `requirements.txt`:
+``` bash
+pip install -r requirements.txt
+```
+
+### Step 2. Run the Parser
+To parse a website, run 
+``` bash
+python3 parser.py [-h] [-d DEPTH] [-n N] [-s SLEEP] [-t] URL
+```
+At its minimum, the script just requires a full url of a website to parse. The list of optional arguments can be retrieved via `python3 parser.py -h`. 
+-  `-h, --help` show this help message and exit
+-  `-d, --depth` depth of search (default=2)
+-  `-n` maximum number of urls to parse (default=None)
+-  `-s --sleep` sleep time between requests (default=0)
+-  `-t` save a tree representation of the site (tree.txt) (default: False)
+
+## Remarks
+- The code in `parser.py` is heavily commented, please refer there for more info.
+- Parser does not use any methods to bypass anti-parsing blockings. It is assumed that given sites are not protected from parsing. The only way to lower the risk of a server-side blocking is to specify the `sleep` parameter.
+- It is possible to see the warnings for each requested url (if any) after the parsing is completed. To do so, print "Y" when prompted.
+- 
+
+## Example
+> :warning: **Results may vary depending on your machine, network and other specifications.**
+Let's parse the [Poetry website](https://python-poetry.org). We'll set the depth at 3 (without max. number of pages) and ask to save a tree representation of the site. Running
+``` bash
+python3 parser.py -d 3 -n 500 -t "https://python-poetry.org"
+```
+we get 2081 pages in almost 23 minutes, and 3 warnings among them. Two of these warnings refer to websites that are blocked in Russia and therefore unreachable, and one is due to incorrect url:
+```
+https://www.patreon.com/	HTTPSConnectionPool(host='www.patreon.com', port=443): Max retries exceeded with url: / (Caused by SSLError(SSLEOFError(8, 'EOF occurred in violation of protocol (_ssl.c:1129)')))
+https://twitter.com/vercel	HTTPSConnectionPool(host='twitter.com', port=443): Max retries exceeded with url: /vercel (Caused by NewConnectionError('<urllib3.connection.HTTPSConnection object at 0x7fecb27cd790>: Failed to establish a new connection: [Errno 60] Operation timed out'))
+9https://github.com/python-poetry/poetry/pull/4942	No connection adapters were found for '9https://github.com/python-poetry/poetry/pull/4942'
+```
+
+`urls.txt` file is a log of all urls that were visited by the parser:
+```
+1 https://python-poetry.org
+2 https://python-poetry.org/docs/plugins/
+3 https://python-poetry.org/blog/
+4 https://python-poetry.org//docs/pyproject/#plugins
+5 https://python-poetry.org/docs/configuration/
+...
+```
+
+`data` directory contains html dumps of all the pages listed in `urls.txt`. We also get a tree representation of the Poetry website (see below).
+
+!!! TREE HERE !!!
+
+
  
---
+++ parser.py
@@ -0,0 +1,251 @@
+import requests
+from requests.exceptions import InvalidSchema, Timeout, ConnectionError, RequestException
+import shutil
+from pathlib import Path
+import os
+from bs4 import BeautifulSoup
+import time
+import argparse
+import urllib.parse
+import itertools
+import enlighten
+from treelib import Tree
+import click
+
+# Define command line arguments
+parser = argparse.ArgumentParser(
+    description='Parse a website and save all html pages.',
+    epilog='''
+    By default, parser.py creates an url.txt file with page ids and their urls
+    and a data folder with html contents of pages (<page id>.html).
+
+    If -t option is enabled, tree.txt file is also created,
+    which contains a tree representation of the website.
+    '''
+)
+parser.add_argument(
+    'url',
+    metavar='URL',
+    help="url of the site to parse",
+    type=str
+    )
+parser.add_argument(
+    '-d',
+    '--depth',
+    help="depth of search (default=2)",
+    type=int,
+    default=2
+    )
+parser.add_argument(
+    '-n',
+    help="maximum number of urls to parse (default=None)",
+    type=int,
+    default=None
+    )
+parser.add_argument(
+    '-s',
+    '--sleep',
+    help="sleep time between requests (default=0)",
+    type=float,
+    default=0
+    )
+parser.add_argument(
+    '-t',
+    help='save a tree representation of the site (tree.txt)',
+    default=False,
+    action=argparse.BooleanOptionalAction
+    )
+
+# Parse arguments from the command line
+args = parser.parse_args()
+url = args.url
+if (urllib.parse.urlsplit(url).scheme == '') or (urllib.parse.urlsplit(url).netloc == ''):
+    raise ValueError(
+        '''
+        Incorrect url is provided.
+        Please specify url in the format "<scheme>://<location>/<path>",
+        e.g. "https://ru.wikipedia.org/"
+        '''
+        )
+depth = args.depth
+n_urls = args.n
+sleep = args.sleep
+return_tree = args.t
+
+# Print info message
+print('Parsing ' + url + ' ...')
+
+
+# Define the parser class to initialise later
+class DeepWebParser:
+    def __init__(self, url, depth=2, n=None, sleep=0, path='./'):
+        self.url = url                      # url to parse
+        self.depth = depth                  # depth of search
+        self.n = n                          # max number of urls
+        self.c = itertools.count(start=1)   # document id counter
+        self.path = path                    # path in which results are saved
+        self.warns = []                     # warnings occured
+        self.tree = []                      # data for site tree
+        self.sleep = sleep                  # sleep time between requests
+
+        # command line page counter
+        counter_format = '{desc}{desc_pad}{count:d} {unit}{unit_pad} (press ctrl+c to interrupt){fill}[{elapsed}]'
+        self.manager = enlighten.get_manager(width=60)
+        self.id_counter = self.manager.counter(
+            total=None,
+            desc='Parsed',
+            unit='pages',
+            counter_format=counter_format
+            )
+
+    def complete_url(self, u, base):
+        '''
+        Return a correct representation of an url,
+        for example if given url is relative.
+        Requires a parent (base) url.
+        '''
+
+        base_scheme = urllib.parse.urlsplit(base).scheme
+        base_netloc = urllib.parse.urlsplit(base).netloc
+        base = base_scheme + '://' + base_netloc
+
+        if (urllib.parse.urlsplit(u).netloc == ''):
+            if '#' in u:
+                return base + '/' + u
+            else:
+                return urllib.parse.urljoin(base, u)
+        elif (urllib.parse.urlsplit(u).scheme == ''):
+            return 'https:' + u
+        else:
+            return u
+
+    def tree_search(self, url, depth, parent_id=None):
+        '''
+        Recursively parse the website.
+        One call corresponds to one url.
+        '''
+
+        time.sleep(self.sleep)  # wait between iterations to prevent bans
+
+        if depth > 0:
+            id = str(next(self.c))              # generate next document id
+            if self.n and int(id) > self.n:     # check condition for max. number of pages
+                return
+
+            # load page, accounting for `requests` package exceptions
+            content = ''
+            try:
+                response = requests.get(
+                    url,
+                    headers=None
+                    )
+                content = response.text
+            except (InvalidSchema, Timeout, ConnectionError) as req_err:
+                self.warns.append(url + '\t' + str(req_err) + '\n')
+            except RequestException:
+                self.warns.append(url + '\t' + 'Unexpected error occured' + '\n')
+
+            self.id_counter.update()            # update command line counter
+
+            # save html contents of current page
+            data_path = os.path.join(self.path, 'data', id+'.html')
+            with open(data_path, 'w') as content_file:
+                content_file.write(content)
+
+            # append id and url to urls.txt
+            with open(os.path.join(self.path, 'urls.txt'), 'a') as urls_file:
+                urls_file.write(id + ' ' + url + '\n')
+
+            # cooking a [very beautiful] soup
+            soup = BeautifulSoup(content, 'html.parser')
+
+            # retrieving page title
+            if soup.find('title') is not None:
+                title = soup.find('title').text
+                title = title[:96]
+            else:
+                title = 'Unknown'
+            title = title.strip()
+
+            # working with fragment identifiers in urls ("#").
+            # urls with different #'s are identical from the html point of view,
+            # but are nevertheless saved separately with different titles.
+            # Titles can be seen in tree.txt file if -t option is enabled.
+            if '#' in url:
+                title = title + ' (#' + url.split('#')[1] + ')'
+            self.tree.append([title, id, parent_id])
+
+            # retrieving all hyperrefs on page
+            urls = soup.find_all('a')
+            urls = [self.complete_url(i['href'], url) for i in urls if i.has_attr('href')]
+
+            # removing "tel:" and "mailto:" links, which are pointless
+            urls = [i for i in urls if ('tel:' not in i) and ('mailto:' not in i)]
+
+            # filtering out self-links
+            urls = [i for i in urls if i != url]
+
+            # removing duplicates
+            urls = list(set(urls))
+
+            # recursively call the function for each url on the page
+            for u in urls:
+                self.tree_search(url=u, depth=depth-1, parent_id=id)
+
+        else:
+            return
+
+    def remove_files(self):
+        '''
+        Remove all data from parsing.
+        '''
+        data_dir = Path(os.path.join(self.path, 'data'))
+
+        if data_dir.exists() and data_dir.is_dir():
+            shutil.rmtree(data_dir)
+        Path(os.path.join(self.path, 'urls.txt')).unlink(missing_ok=True)
+        Path(os.path.join(self.path, 'tree.txt')).unlink(missing_ok=True)
+
+    def end_cycle(self):
+        '''
+        Happily end the process.
+        Ask if user wants to see the errors occured during parsing.
+        '''
+        if click.confirm(f'\n\n{len(self.warns)} warnings occured. Want to see them?', default=True):
+            print(*self.warns, sep='')
+
+    def parse(self):
+        '''
+        Execute parsing process
+        '''
+        self.remove_files()
+        Path(
+            os.path.join(self.path, 'data')
+            ).mkdir(parents=True, exist_ok=True)
+        self.tree_search(self.url, self.depth)
+        self.end_cycle()
+
+    def save_tree(self):
+        '''
+        Save a tree representation of the website.
+        '''
+        tree = Tree()
+        for node, id, parent in self.tree:
+            tree.create_node(node, id, parent)
+
+        tree.save2file(os.path.join(self.path, 'tree.txt'))
+
+
+try:
+    parser = DeepWebParser(
+        url=url,
+        depth=depth,
+        n=n_urls,
+        sleep=sleep
+        )
+    parser.parse()
+except KeyboardInterrupt:
+    parser.end_cycle()
+
+if return_tree:
+    parser.save_tree()
